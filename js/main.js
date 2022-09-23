b71f11ea0631c1d1ce98f8a116db7102937e3d12
Mirka <mirsdwk@gmail.com>
2022-09-23 12:46:27 +0200

    cambio el abrir y cerrar boca de la rana de css a js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let bocaCerrada= document.getElementById("boca-cerrada");
+let bocaAbierta= document.getElementById("boca-abierta");
+
+
+bocaAbierta.style.display='none';
+/* bocaAbierta.style.display='block'; */ 
